391ba16443c776f84af53e8b6c9e9df1f5620f7a
Alfred Besong <besongegbealfred@gmail.com>
2026-07-16 22:25:26 +0100

    changes made

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -1,7 +1,7 @@
 class AppStrings {
   static const appName = 'Smart Waste Management';
   static const appSubtitle = 'Citizen report and admin tracking system';
-  static const backendBaseUrl = 'http://localhost:8000/api ';//for local testing
+  static const backendBaseUrl = 'http://localhost:8000/api';//for local testing
   //https://waste-management-system-02zx.onrender.com for live production testing
 }
 
